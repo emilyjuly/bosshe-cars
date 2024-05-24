f7a21c5736262753f2b4e707f9dcae0dbada46f1
emilyjuly <emilyjulygd@gmail.com>
2024-05-23 23:00:41 -0300

    Adiciona configurações iniciais

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <section class="h-screen" id="inicio">
+        <div class="">
+            <img src="../assets/images/carro-pagina-inicial.png" alt="Fotografia de um carro" />
+        </div>
+      </section>
+      <section class="h-screen" id="quem-somos">
+        <p>Conteúdo da seção Quem Somos</p>
+      </section>
+      <section class="h-screen" id="catalogo">
+        <p>Conteúdo da seção Catálogo</p>
+      </section>
+      <section class="h-screen" id="fale-conosco">
+      </section>
+    </div>
+  </template>
+  
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`,
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray': '464646',
+        'medium_gray': '939393',
+        'light_gray': '#C0C0C0',
+        'white': '#ECECEC',
+        'light_black': '#1D1D1D',
+        'transparent_gray': 'rgba(148, 148, 148, 0.76)',
+        'beige': '#C5AC94',
+        'transparent_black': 'rgba(24, 24, 24, 0.38)',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
+
