119f7eed6c1451f7a43e3530a91f2f6e9c049054
Emily Domingos <emily.domingos@e-unicred.com.br>
2024-05-24 15:40:09 -0300

    Inicia preparação da tela inicial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
-  colorMode: {
-    classSuffix: "",
-    preference: "light",
-    fallback: "light",
-  },
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+    colorMode: {
+        classSuffix: "",
+        preference: "light",
+        fallback: "light",
     },
-  },
+    devtools: {enabled: false},
+    css: ["~/assets/css/main.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
-      <section class="h-screen" id="inicio">
-        <div class="">
-            <img src="../assets/images/carro-pagina-inicial.png" alt="Fotografia de um carro" />
-        </div>
-      </section>
-      <section class="h-screen" id="quem-somos">
-        <p>Conteúdo da seção Quem Somos</p>
-      </section>
-      <section class="h-screen" id="catalogo">
-        <p>Conteúdo da seção Catálogo</p>
-      </section>
-      <section class="h-screen" id="fale-conosco">
-      </section>
+        <section class="h-screen" id="inicio">
+            <div>
+                <img
+                    class="absolute top-0 right-0 h-full object-cover w-3/4"
+                    src="../assets/images/carro-pagina-inicial.svg"
+                    alt="Imagem de um carro"
+                />
+            </div>
+            <div>
+                <img
+                    class="absolute z-20 bg-transparent"
+                    style="top: 20rem; left: 13rem;"
+                    src="../assets/images/palavras-inicio.png"
+                    alt="Palavras sobre a imagem do carro"
+                />
+            </div>
+        </section>
+        <section class="h-screen" id="quem-somos">
+            <p>Conteúdo da seção Quem Somos</p>
+        </section>
+        <section class="h-screen" id="catalogo">
+            <p>Conteúdo da seção Catálogo</p>
+        </section>
+        <section class="h-screen" id="fale-conosco">
+        </section>
     </div>
-  </template>
-  
+</template>
+<script setup lang="ts">
+</script>
